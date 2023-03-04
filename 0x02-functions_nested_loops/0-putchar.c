8ca1bbd898b8270406b3f4b07069f8f2fea03d45
furahacodes@gmail.com <furahacodes@gmail.com>
2023-03-04 07:19:35 -0800

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
-
-int main() {
-	printf("_putchar\n);
+#include "main.h"
+/**
+ * main - write a program that prints "_putchar"
+ * Description: write a c program
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return 0;
 }
